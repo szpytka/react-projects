d9f8564ff8b90ba071b12f53c8e84801dacbfad2
szpytka <jakub.szpytka@gmail.com>
2023-03-15 16:54:50 +0100

    testimonials project

--- 30_projects_inside_src_folder/src/App.js
+++ 30_projects_inside_src_folder/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
-import RandomizeColors from './projects/02-randomize-colors/RandomizeColors';
-import LikePhotoApp from './projects/03-like-my-photo/LikePhotoApp';
+// import RandomizeColors from './projects/02-randomize-colors/RandomizeColors';
+// import LikePhotoApp from './projects/03-like-my-photo/LikePhotoApp';
 // import EsignatureApp from './projects/01-e-signature-app/EsignatureApp';
+import Testimonials from './projects/04-testimonials/Testimonials';
 
 function App() {
   return (
     <div className="App">
       {/* <EsignatureApp /> */}
       {/* <RandomizeColors /> */}
-      <LikePhotoApp />
+      {/* <LikePhotoApp /> */}
+      <Testimonials />
     </div>
   );
 }
