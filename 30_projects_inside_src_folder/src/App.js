600babb56ddeed0cb4946191fb5d043ac62c45f4
szpytka <jakub.szpytka@gmail.com>
2023-03-15 17:28:08 +0100

    temperature controller project

--- 30_projects_inside_src_folder/src/App.js
+++ 30_projects_inside_src_folder/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import TemperatureController from './projects/06-temperature-controller/TemperatureController';
 // import RandomizeColors from './projects/02-randomize-colors/RandomizeColors';
 // import LikePhotoApp from './projects/03-like-my-photo/LikePhotoApp';
 // import EsignatureApp from './projects/01-e-signature-app/EsignatureApp';
 // import Testimonials from './projects/04-testimonials/Testimonials';
-import Alerts from './projects/05-alerts/Alerts';
+// import Alerts from './projects/05-alerts/Alerts';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       {/* <RandomizeColors /> */}
       {/* <LikePhotoApp /> */}
       {/* <Testimonials /> */}
-      <Alerts />
+      {/* <Alerts /> */}
+      <TemperatureController />
     </div>
   );
 }
