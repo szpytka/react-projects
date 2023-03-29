5f1839ba83473e177ece447f8107140d320fe193
szpytka <jakub.szpytka@gmail.com>
2023-03-29 13:37:53 +0200

    color boxes

---
+++ 10-colors-app/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Palette from './Palette';
+import seedColors from './seedColors';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Palette {...seedColors[3]} />
+      </div>
+    );
+  }
+}
