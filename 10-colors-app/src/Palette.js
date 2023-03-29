58b1f96232b57d8e1cd6326c13e60370d7ed1cf1
szpytka <jakub.szpytka@gmail.com>
2023-03-29 17:24:35 +0200

    added navbar:

--- 10-colors-app/src/Palette.js
+++ 10-colors-app/src/Palette.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
 import './Palette.css';
+import Navbar from './Navbar';
 export default class Palette extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      level: 500,
+    };
+    this.changeLevel = this.changeLevel.bind(this);
+  }
+  changeLevel(level) {
+    this.setState({ level });
+  }
   render() {
-    const colorBoxes = this.props.colors.map((color) => (
-      <ColorBox background={color.color} name={color.name} />
+    const { colors } = this.props.palette;
+    const { level } = this.state;
+    const colorBoxes = colors[level].map((color) => (
+      <ColorBox background={color.hex} name={color.name} />
     ));
     return (
       <div className="Palette">
+        <Navbar level={level} changeLevel={this.changeLevel} />
         {/* navbar goes here */}
         <div className="Palette-colors">{colorBoxes}</div>
         {/* footer eventually */}
